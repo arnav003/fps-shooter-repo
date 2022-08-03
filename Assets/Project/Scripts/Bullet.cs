789b746bc13ec1e2b05c883240d607382e9854e0
arnav003 <lalaarnav003@gmail.com>
2022-08-04 01:40:42 +0530

    Initial commit

---
+++ Assets/Project/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public float LifeDuration = 1f;
+
+    private float timer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = LifeDuration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        { 
+            Destroy(this.gameObject);
+        }
+    }
+}
